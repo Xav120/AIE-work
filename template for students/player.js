64a196c25622c8461a3a00ffe903a599d6ec8e33
Xav120 <xavier.x909@gmail.com>
2015-10-02 15:42:25 +1000

    Player update
    the shooting has been fixed but in turn the animation is broken

--- template for students/player.js
+++ template for students/player.js
@@ -54,7 +54,7 @@ var Player = function()
 	[73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87]);
 	
 	this.image = document.createElement("img");
-	for (var idx = 0;  idx < ANIM_MAX; idx++)
+		for (var idx = 0; idx < ANIM_MAX; idx++)
 	{
 		this.sprite.setAnimationOffset(idx, -55, -87);
 	}
@@ -64,32 +64,35 @@ var Player = function()
 	
 	this.width = 159;
 	this.height = 163;
-	
+
 	this.velocity_x = 0;
 	this.velocity_y = 0;
 	
 	this.falling = true;
 	this.jumping = true;
-	this.direction = RIGHT;
-	this.direction = LEFT;
-	this.lives = 3
 	
+	this.direction = LEFT;
+	
+	this.lives = 3;
 	this.respawn_x = this.x;
 	this.respawn_y = this.y;
 	
-	this.life_image = document.createElement("img");
-	this.life_image.src = "life.png";
-	
+	this.lives_image = document.createElement("img");
+	this.lives_image.src = "life.png";
+
 	this.max_bullets = 50;
 	this.bullets = [];
-	this.cur_bullets_idx = 0;
+	this.cur_bullet_index = 0;
 	for(var idx = 0; idx < this.max_bullets; idx++)
 	{
 		this.bullets[idx] = new Bullet();
 	}
 	
 	this.shoot_cooldown = 0.0;
-	this.shoot_timer = 0.01;
+	this.shoot_timer = 0.1;
+	
+	var self = this;
+	this.is_jump_sfx_playing = false;
 	
 	this.jump_sfx = new Howl(
 	{
@@ -97,7 +100,7 @@ var Player = function()
 		buffer : true,
 		volume : 1,
 		onend : function(){
-		self.is_jump_sfx_playing = false;
+			self.is_jump_sfx_playing = false;
 		}
 	});
 	
@@ -107,9 +110,10 @@ var Player = function()
 		buffer : true,
 		volume : 1,
 		onend : function(){
-			self.is_shoot_sfx_playing = false
+			self.is_shoot_sfx_playing = false;
 		}
 	});
+	
 }
 
 Player.prototype.update = function(deltaTime)
@@ -119,90 +123,84 @@ Player.prototype.update = function(deltaTime)
 	var left, right, jump;
 	left = right = jump = false;
 	
-	if (keyboard.isKeyDown(keyboard.KEY_LEFT))
+	if (keyboard.isKeyDown(37))
 	{
-		left = true
+		left = true;
 		this.direction = LEFT;
-		if (this.sprite.currentAnimation != ANIM_WALK_LEFT && !this.shooting)
-			this.sprite.setAnimation(ANIM_WALK_LEFT)
+		if (this.sprite.currentAnimation != ANIM_WALK_LEFT && !this.jumping)
+			this.sprite.setAnimation(ANIM_WALK_LEFT);
 	}
 	else if (keyboard.isKeyDown(keyboard.KEY_RIGHT))
 	{
-		right = true
+		right = true;
 		this.direction = RIGHT;
-		if (this.sprite.currentAnimation != ANIM_WALK_RIGHT && !this.shooting)
-			this.sprite.setAnimation(ANIM_WALK_RIGHT)
-
+		if (this.sprite.currentAnimation != ANIM_WALK_RIGHT && !this.jumping)
+			this.sprite.setAnimation(ANIM_WALK_RIGHT);
 	}
 	else
 	{
-		if (!this.jumping && !this.falling && !this.shooting)
+		if (!this.jumping && !this.falling)
 		{
 			if (this.direction == LEFT)
 			{
 				if (this.sprite.currentAnimation != ANIM_IDLE_LEFT)
 					this.sprite.setAnimation(ANIM_IDLE_LEFT);
-			}
-			else if (this.direction == RIGHT)
+			}	
+			else
 			{
 				if (this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
-				this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+					this.sprite.setAnimation(ANIM_IDLE_RIGHT);
 			}
 		}
 	}
-	
-		if (keyboard.isKeyDown(keyboard.KEY_SHIFT) && !jump)
-	{
-		this.shooting = true;
-		if (this.direction == LEFT)
-		{
-			if (this.sprite.currentAnimation != ANIM_SHOOT_LEFT )
-				this.sprite.setAnimation(ANIM_SHOOT_LEFT);
-		}
-		else if (this.direction == RIGHT)
-		{
-			if (this.sprite.currentAnimation != ANIM_SHOOT_RIGHT)
-				this.sprite.setAnimation(ANIM_SHOOT_RIGHT);
-		}
-	}
-	else if (this.shooting)
-		this.shooting = false;
-
-	
 	if (keyboard.isKeyDown(keyboard.KEY_SPACE))
 	{
-		jump = true
+		jump = true;
 		if (left == true)
-			this.sprite.setAnimation(ANIM_JUMP_LEFT)
+			this.sprite.setAnimation(ANIM_JUMP_LEFT);
 		if (right == true)
-			this.sprite.setAnimation(ANIM_JUMP_RIGHT)
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT);
 	}
 	
-	if (keyboard.isKeyDown(keyboard.KEY_SHIFT))
+	if(keyboard.isKeyDown(keyboard.KEY_SHIFT))
 	{
-		if (this.shoot_cooldown <= 0)
+		if (!this.jumping && !this.falling)
 		{
-			if (!this.is_shoot_sfx_playing)
+			if (left == true)
 			{
-				this.shoot_sfx
-				this.shoot_sfx.play();
-				this.is_shoot_sfx_playing = true;
+				if (this.sprite.currentAnimation != ANIM_SHOOT_LEFT)
+					this.sprite.setAnimation(ANIM_SHOOT_LEFT);
 			}
-			
-			var jitter = Math.random() * 0.2 - 0.1;
-			
-			if (this.direction == left)			
-				this.bullets[this.cur_bullets_idx].fire(this.x, this.y, -1, jitter);
-			else
-				this.bullets[this.cur_bullets_idx].fire(this.x, this.y, 1, jitter);
-			
-			this.shoot_cooldown = this.shoot_timer;
-			
-			this.cur_bullet_index ++;
-			if (this.cur_bullets_index >= this.max_bullets)
+			else if (right == true)
 			{
-				this.cur_bullets_index = 0;
-			}	
+				if (this.sprite.currentAnimation != ANIM_SHOOT_RIGHT)
+				{
+					this.sprite.setAnimation(ANIM_SHOOT_RIGHT);
+				}
+			}
+			this.shoot = true;
+		
+			if (this.shoot_cooldown <= 0 && (left == true || right == true))
+			{
+				if (!this.is_shoot_sfx_playing)
+				{
+					this.shoot_sfx.play();
+					this.is_shoot_sfx_playing = true;
+				}
+				
+				var jitter = Math.random() * 0.2 - 0.1;
+				
+				if (this.direction == LEFT)
+					this.bullets[this.cur_bullet_index].fire(this.x, this.y, -1, jitter);
+				else
+					this.bullets[this.cur_bullet_index].fire(this.x + 70, this.y - 10, 1, jitter);
+				
+				this.shoot_cooldown = this.shoot_timer;
+				
+				this.cur_bullet_index ++;
+				if (this.cur_bullet_index >= this.max_bullets)
+					this.cur_bullet_index = 0;
+			}
 		}
 	}
 	if (this.shoot_cooldown > 0)
@@ -212,65 +210,63 @@ Player.prototype.update = function(deltaTime)
 	{
 		this.bullets[i].update(deltaTime);
 	}
-	
-	
-	
+
 	var wasleft = this.velocity_x < 0;
 	var wasright = this.velocity_x > 0;
 	var falling = this.falling;
 	var ddx = 0;
 	var ddy = GRAVITY;
 	
-	if(left)
+	if (left)
 		ddx = ddx - ACCEL;
 	else if (wasleft)
 		ddx = ddx + FRICTION;
 	
-	if(right)
+	if (right)
 		ddx = ddx + ACCEL;
 	else if (wasright)
 		ddx = ddx - FRICTION;
 	
-	if(jump && !this.jumping && !falling)
+	if (jump && !this.jumping && !falling)
 	{
 		this.jump_sfx.play();
-		this.is_jump_sfx_playing = true
+		this.is_jump_sfx_playing = true;
+		
 		ddy = ddy - JUMP;
 		this.jumping = true;
-		if(this.direction = LEFT)
+		if (this.direction == LEFT)
 		{
-			if(this.sprite.currentAnimation != ANIM_JUMP_LEFT)
 				this.sprite.setAnimation(ANIM_JUMP_LEFT);
 		}
 		else
 		{
-			if(this.sprite.currentAnimation != ANIM_JUMP_RIGHT)
 				this.sprite.setAnimation(ANIM_JUMP_RIGHT);
 		}
+
 	}
 	
-		
-	this.x = Math.floor(this.x +(deltaTime * this.velocity_x))
-	this.y = Math.floor(this.y +(deltaTime * this.velocity_y))
-	this.velocity_x = bound(this.velocity_x + (deltaTime * ddx), - MAXDX, MAXDX);
-	this.velocity_y = bound(this.velocity_y + (deltaTime * ddy), - MAXDY, MAXDY);
+	this.x = Math.floor(this.x + (deltaTime * this.velocity_x));
+	this.y = Math.floor(this.y + (deltaTime * this.velocity_y));
+	this.velocity_x = bound(this.velocity_x + (deltaTime * ddx), -MAXDX, MAXDX);
+	this.velocity_y = bound(this.velocity_y + (deltaTime * ddy), -MAXDY, MAXDY);
 
-	if((wasleft && (this.velocity_x > 0)) ||
+	if ((wasleft && (this.velocity_x > 0)) ||
 		(wasright && (this.velocity_x < 0)))
 	{
 		this.velocity_x = 0;
 	}
-		
+	
+
 	var tx = pixelToTile(this.x);
 	var ty = pixelToTile(this.y);
 	var nx = (this.x) % TILE;
 	var ny = (this.y) % TILE;
-		
+	
 	var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
 	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
 	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
 	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
-				
+	
 	if (this.velocity_y > 0)
 	{
 		if ((celldown && !cell) || (celldiag && !cellright && nx))
@@ -284,10 +280,13 @@ Player.prototype.update = function(deltaTime)
 	}
 	else if (this.velocity_y < 0)
 	{
-		if ((cell && !celldown) || (cellright && !celldiag && nx) )
+		if ((cell && !celldown) || (cellright && !celldiag && nx))
 		{
 			this.y = tileToPixel(ty + 1);
 			this.velocity_y = 0;
+			cell = celldown;
+			cellright = celldiag;
+			ny = 0
 		}
 	}
 	
@@ -297,7 +296,7 @@ Player.prototype.update = function(deltaTime)
 		{
 			this.x = tileToPixel(tx);
 			this.velocity_x = 0;
-		}	
+		}			
 	}
 	else if (this.velocity_x < 0)
 	{
@@ -305,30 +304,30 @@ Player.prototype.update = function(deltaTime)
 		{
 			this.x = tileToPixel(tx + 1);
 			this.velocity_x = 0;
-		}	
+		}
 	}
-	if(this.y> MAP.th * TILE + 100)
+	
+	if (this.y > MAP.th * TILE + 100)
 	{
 		this.x = this.respawn_x;
 		this.y = this.respawn_y;
 		this.lives --;
 	}
+	
 }
-		
-
 
 Player.prototype.draw = function(_cam_x, _cam_y)
 {
-		for (var idx = 0; idx < this.lives; idx++)
-	{
-		context.drawImage(this.life_image, 40 + idx * (this.life_image.width), 40);
-	}
+	this.sprite.draw(context, this.x - _cam_x,
+							  this.y - _cam_y);
 	
-	this.sprite.draw(context, this.x - _cam_x, this.y - _cam_y);
+	for (var idx = 0; idx < this.lives; idx++)
+	{
+		context.drawImage(this.lives_image, 40 + idx * (this.lives_image.width), 40);
+	}
 	
 	for (var idx = 0; idx < this.max_bullets; idx++)
 	{
 		this.bullets[idx].draw(_cam_x, _cam_y);
 	}
-	
 }
